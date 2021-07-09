09fcb6f19bd5606d8d866d2f831ab5cf1f997d2a
atecristian11 <86316490+atecristian11@users.noreply.github.com>
2021-07-09 10:59:25 -0500

    Add files via upload

---
+++ calculadora/calculadora.js
@@ -0,0 +1,63 @@
+let display = document.getElementById("display");
+// let valorAnterior = document.getElementById("valorAnterior");
+// let valorAnterior = document.getElementById("valorActual");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
+
+btn1.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "1";
+};
+
+btn2.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "2";
+};
+
+btn3.onclick = function () {
+    console.log(display.innerHTML);
+    display.innerHTML += "3";
+};
+
+btn4.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "4";
+};
+
+btn5.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "5";
+};
+
+btn6.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "6";
+};
+
+btn7.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "7";
+};
+
+btn8.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "8";
+};
+
+btn9.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "9";
+};
+
+btn0.onclick = function () {
+  console.log(display.innerHTML);
+  display.innerHTML += "0";
+};
